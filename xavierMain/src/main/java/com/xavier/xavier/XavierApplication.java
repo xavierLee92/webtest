32fdcc2d4d067d135909da296767181707ac46b8
DESKTOP-0CQTLCV\admin <lixinfei315@126.com>
2018-07-26 21:39:25 +0800

    add   main

---
+++ xavierMain/src/main/java/com/xavier/xavier/XavierApplication.java
@@ -0,0 +1,12 @@
+package com.xavier.xavier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XavierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XavierApplication.class, args);
+    }
+}
